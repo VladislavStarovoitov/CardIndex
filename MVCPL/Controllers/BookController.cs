055d62c1c3e258d853cac8559dce14fd3f2ce6fc
VladislavStarovoitov <VladislavStarovoitov>
2016-08-29 02:17:09 +0300

    Add UnitOfWork in controller

--- MVCPL/Controllers/BookController.cs
+++ MVCPL/Controllers/BookController.cs
@@ -4,13 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BLL.Interface.Services;
+using BLL.Mappers;
 
 namespace MVCPL.Controllers
 {
     public class BookController : Controller
     {
-        // GET: Admin
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+             
         public ActionResult Index()
         {
             return View();
@@ -28,7 +35,14 @@ namespace MVCPL.Controllers
             byte[] image = new byte[file.ContentLength];
             file.InputStream.Read(image, 0, file.ContentLength);
             book.Image = image;
+           
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _bookService.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
