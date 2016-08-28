bfa94fbbcc6098102ba98934c9a80552aa6a426b
VladislavStarovoitov <VladislavStarovoitov>
2016-08-29 01:51:55 +0300

    Add binds in DependencyResolver

--- DependencyResolver/ResolverConfig.cs
+++ DependencyResolver/ResolverConfig.cs
@@ -1,9 +1,16 @@
 ﻿using Ninject;
+using Ninject.Web.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL.Interface.Repositories;
+using DAL.Repositories;
+using ORM;
+using BLL.Interface.Services;
+using BLL.Services;
 
 namespace DependencyResolver
 {
@@ -11,7 +18,10 @@ namespace DependencyResolver
     {
         public static void Configurate(this IKernel kernel)
         {
+            kernel.Bind<DbContext>().To<CardIndex>().InRequestScope();
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
 
+            kernel.Bind<IBookService>().To<BookService>();
         }
     }
 }
